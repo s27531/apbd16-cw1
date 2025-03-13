66c8841e34555d5d93cb3ee9e6c32e3c7c6ada92
s27531 <s27531@pjwstk.edu.pl>
2025-03-13 15:24:39 +0100

    Added GetAverage() method.

--- Zad1/Zad1/Program.cs
+++ Zad1/Zad1/Program.cs
@@ -1,4 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
-Console.WriteLine("abc");
-Console.WriteLine("cba");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("abc");
+        Console.WriteLine("cba");
+
+        int[] nums = { 1, 2, 3, 4, 5, 2 };
+        double avg = GetAverage(nums);
+        Console.WriteLine("Avg: " + avg);
+
+    }
+
+    public static double GetAverage(int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++) {
+            sum += numbers[i];
+        }
+
+        return sum / (double) numbers.Length;
+    }
+}
