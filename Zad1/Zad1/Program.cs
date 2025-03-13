db4cbdceb153c13d65ce74f2c33ecf20feafc87d
s27531 <s27531@pjwstk.edu.pl>
2025-03-13 15:39:01 +0100

    Changed var name.

--- Zad1/Zad1/Program.cs
+++ Zad1/Zad1/Program.cs
@@ -15,8 +15,8 @@
     public static double GetAverage(int[] numbers)
     {
         int sum = 0;
-        for (int i = 0; i < numbers.Length; i++) {
-            sum += numbers[i];
+        for (int c = 0; c < numbers.Length; c++) {
+            sum += numbers[c];
         }
 
         return sum / (double) numbers.Length;
